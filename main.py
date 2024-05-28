aa8a8554b6c825017116732cbdbbd675cc40607c
KennetSoto <kennet.soto@hotmail.com>
2024-05-28 19:58:25 -0300

    v 1.0

---
+++ main.py
@@ -0,0 +1,31 @@
+# Crie um dicionário em Python com os seguintes dados de uma pessoa:
+# - Nome
+# - CPF
+# - RG
+# - Data de Nascimento
+# - Gênero
+# - E-mail
+# - Telefone
+# - Tipo sanguíneo
+# - Profissão
+# - Empresa
+
+# O programa deverá exibir os dados da pessoa no console.
+
+print('Insira os dados do usuário')
+pessoa = {
+    "Nome": input("Digite o nome: "),
+    "CPF": input("Digite o CPF: "),
+    "RG": input("Digite o RG: "),
+    "Data de Nascimento": input("Digite a data de nascimento (dd/mm/aaaa): "),
+    "Gênero": input("Digite o gênero (M/F): "),
+    "E-mail": input("Digite o e-mail: "),
+    "Telefone": input("Digite o telefone: "),
+    "Tipo sanguíneo": input("Digite o tipo sanguíneo: "),
+    "Profissão": input("Digite a profissão: "),
+    "Empresa": input("Digite a empresa: ")
+}
+
+print('\nDados do usuário')
+for chave, valor in pessoa.items():
+    print(f"{chave}: {valor}")
